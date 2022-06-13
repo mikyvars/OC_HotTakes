e6f5007d8ae3c9708b5fe4dc3872a1d7bc3de1b1
Michael YVARS <michaelyvars1@gmail.com>
2022-06-13 18:01:07 +0200

    fix cors error

--- app.js
+++ app.js
@@ -15,13 +15,14 @@ const app = express()
 app.use(express.json())
 
 // secure server by setting various HTTP headers 
-app.use(helmet())
+app.use(helmet());
 
 // secure CORS errors
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
 	next();
 });
 
