d925afe249f48fe150e91ccb1992d3bd74dbf288
Michael YVARS <michaelyvars1@gmail.com>
2022-06-13 18:10:32 +0200

    add secure to check user id before change sauce

---
+++ middlewares/userId.js
@@ -0,0 +1,18 @@
+const webToken = require('jsonwebtoken')
+const Sauce = require('../models/Sauce')
+
+module.exports = (req, res, next) => {
+    Sauce.findOne({_id: req.params.id})
+        .then(sauce => {
+            const token = req.headers.authorization.split(' ')[1]
+            const decodedToken = webToken.verify(token, 'RANDOM_TOKEN_SECRET')
+            const userId = decodedToken.userId
+
+            if(sauce.userId && sauce.userId == userId) {
+                next()
+            } else {
+                res.status(403).json({message: "Requête non authorisée"})
+            }
+        })
+        .catch(error => res.status(401).json({error}))
+}
--- routes/sauce.js
+++ routes/sauce.js
@@ -3,13 +3,14 @@ const router = express.Router()
 const sauceController = require('../controllers/sauce')
 
 const auth = require('../middlewares/auth')
+const userId = require('../middlewares/userId')
 const multer = require('../middlewares/multer-config')
 
 router.get('/', auth, sauceController.getSauces)
 router.get('/:id', auth, sauceController.getOneSauce)
 router.post('/', auth, multer, sauceController.addSauce)
-router.put('/:id', auth, multer, sauceController.modifySauce)
-router.delete('/:id', auth, sauceController.deleteSauce)
+router.put('/:id', auth, userId, multer, sauceController.modifySauce)
+router.delete('/:id', auth, userId, sauceController.deleteSauce)
 router.post('/:id/like', auth, sauceController.likeSauce)
 
 module.exports = router
